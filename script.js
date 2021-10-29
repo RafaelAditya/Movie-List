5219503927025bc262fd70b6428371a1cd4dc35d
Rafael Aditya Cahyo W <64288484+RafaelAditya@users.noreply.github.com>
2021-10-29 15:22:57 +0700

    Commit first web launch

---
+++ script.js
@@ -0,0 +1,93 @@
+//TMDB
+
+const API_KEY = 'api_key=06c811b8f8bea0dbea819766de2c1113';
+const BASE_URL = 'https://api.themoviedb.org/3';
+const API_URL = BASE_URL + "/discover/movie?sort_by=popularity.desc&" + API_KEY;
+const IMG_URL = 'https://image.tmdb.org/t/p/w500';
+const searchURL = BASE_URL + '/search/movie?' + API_KEY;
+
+const main = document.getElementById('main');
+const form = document.getElementById('form');
+const search = document.getElementById('search');
+const detail_container = document.getElementById('detail-container');
+
+getMovies(API_URL);
+
+function getMovies(url){
+    fetch(url).then(res => res.json()).then(data => {
+        showMovies(data.results);
+    })
+}
+
+function showMovies(data) {
+    main.innerHTML = '';
+    detail_container.innerHTML='';
+
+    data.forEach(movie => {
+        const {title, poster_path, vote_average, overview} = movie;
+        const movieEl = document.createElement('div');
+        movieEl.classList.add('movie');
+        movieEl.innerHTML = `
+            <img src="${IMG_URL + poster_path}" alt="${title}"></img>
+
+            <div class="movie-info">
+                <h3>${title}</h3>
+                <span class="${getColor(vote_average)}"> ${vote_average}
+                </span>
+            </div>
+    
+            <div class="overview">
+                <a href="#${title}">See details</a>
+            </div>
+        `
+
+        main.appendChild(movieEl);
+    })
+
+    data.forEach(movie => {
+        const {title, poster_path, vote_average, overview} = movie;
+        const movieEl = document.createElement('div');
+        movieEl.classList.add('movie');
+        movieEl.innerHTML = `
+
+        <div class="movie-details">
+            <div class="image-container" id="${title}">
+                <img src="${IMG_URL + poster_path}" alt="${title}">
+            </div>
+
+            <div class="detail-overview">
+                <h1>${title}</h1>
+                <p>${overview}</p>
+                <a class="back-button" href="#form">Back</a>
+            </div>
+        </div>
+        `
+
+        detail_container.appendChild(movieEl);
+    })
+}
+
+function getColor(vote){
+    if (vote>=8){
+        return 'green'
+    }
+    else if (vote>=5){
+        return 'orange'
+    }
+    else{
+        return 'red'
+    }
+}
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    const searchTerm = search.value;
+
+    if (searchTerm) {
+        getMovies(searchURL+'&query='+searchTerm);
+    }else{
+        getMovies(API_URL);
+    } 
+
+})
